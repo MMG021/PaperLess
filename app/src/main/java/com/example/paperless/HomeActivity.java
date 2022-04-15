4c53c3ba88c0043d03194a5d6a49136c764a3f0d
Mohsin Munir <mohsingoraya313@outlook.com>
2022-04-15 20:04:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/paperless/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.example.paperless;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    // creating a variable for our text view..
+    private TextView userNameTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // initializing our variables
+        userNameTV = findViewById(R.id.idTVUserName);
+
+        // getting data from intent.
+        String name = getIntent().getStringExtra("username");
+
+        // setting data to our text view.
+        userNameTV.setText(name);
+    }
+}
